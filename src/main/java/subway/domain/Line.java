58b3abd5a597e83289a1d8419212ace1f0ab4f2b
travelbeeee <sochun1518@naver.com>
2021-07-01 00:30:09 +0900

    Line 클래스 기능 구현

--- src/main/java/subway/domain/Line.java
+++ src/main/java/subway/domain/Line.java
@@ -1,7 +1,10 @@
 package subway.domain;
 
+import java.util.ArrayList;
+
 public class Line {
     private String name;
+    private ArrayList<Station> stations = new ArrayList<>();
 
     public Line(String name) {
         this.name = name;
@@ -12,4 +15,28 @@ public class Line {
     }
 
     // 추가 기능 구현
+    public ArrayList<Station> getStations() {
+        return stations;
+    }
+
+    public void addStation(Station station) {
+        if (this.stations.contains(station)) {
+            // 에러 상황! --> 추가하려는 역이 이미 해당 노선에 존재합니다.
+            return;
+        }
+        // 양방향 추가
+        stations.add(station);
+        station.addLine(this);
+    }
+
+    public void removeStation(Station station) {
+        if (!this.stations.contains(station)) {
+            // 에러 상황! --> 삭제하려는 역이 해당 노선에 존재하지 않습니다.
+            return;
+        }
+        // 양방향 삭제
+        stations.remove(station);
+        station.removeLine(this);
+    }
+
 }
