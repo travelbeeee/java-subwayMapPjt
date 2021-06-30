5c153a6cc56d9321680bfc2c1e7ecd7b4292ad88
travelbeeee <sochun1518@naver.com>
2021-07-01 00:21:39 +0900

    Station 클래스 기능 구현

--- src/main/java/subway/domain/Station.java
+++ src/main/java/subway/domain/Station.java
@@ -1,7 +1,10 @@
 package subway.domain;
 
+import java.util.ArrayList;
+
 public class Station {
     private String name;
+    private ArrayList<Line> lines = new ArrayList<>();
 
     public Station(String name) {
         this.name = name;
