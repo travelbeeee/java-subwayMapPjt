5f203def42a2fce14b7a45b643accaba15cde6d7
travelbeeee <sochun1518@naver.com>
2021-07-15 22:35:33 +0900

    Logger 메소드 명 정의

--- src/main/java/subway/domain/Logger.java
+++ src/main/java/subway/domain/Logger.java
@@ -1,11 +1,10 @@
 package subway.domain;
 
 public class Logger {
-    public void printInfoMessage(String message){
+    public void info(String message){
         System.out.println("[INFO] " + message);
     }
-
-    public void printErrorMessage(String message){
+    public void error(String message){
         System.out.println("[ERROR] " + message);
     }
 }
