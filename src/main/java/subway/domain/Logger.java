d0f57024545436fb24c5ab7d45fb2d897a405bb1
travelbeeee <sochun1518@naver.com>
2021-07-16 17:05:32 +0900

    Logger 클래스 static으로 선언

--- src/main/java/subway/domain/Logger.java
+++ src/main/java/subway/domain/Logger.java
@@ -1,10 +1,10 @@
 package subway.domain;
 
 public class Logger {
-    public void info(String message){
+    public static void info(String message){
         System.out.println("[INFO] " + message);
     }
-    public void error(String message){
+    public static void error(String message){
         System.out.println("[ERROR] " + message);
     }
 }
