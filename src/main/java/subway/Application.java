80c4a30b412ec68a7bc2ae3726184416339368f3
travelbeeee <sochun1518@naver.com>
2021-07-16 17:06:24 +0900

    Applicaiton 틀 개발중

--- src/main/java/subway/Application.java
+++ src/main/java/subway/Application.java
@@ -1,9 +1,11 @@
 package subway;
 
 import subway.domain.LineRepository;
-import subway.domain.Logger;
 import subway.domain.StationRepository;
+import subway.domain.View;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Application {
@@ -12,36 +14,16 @@ public class Application {
         // TODO: 프로그램 구현
         LineRepository lineRepository = new LineRepository();
         StationRepository stationRepository = new StationRepository();
-        Logger logger = new Logger();
+
+        init(lineRepository, stationRepository);
+
         String choice;
+    }
 
-        while(true){
-            System.out.println("## 메인 화면");
-            System.out.println("1. 역 관리");
-            System.out.println("2. 노선 관리");
-            System.out.println("3. 구간 관리");
-            System.out.println("4. 지하철 노선도 출력");
-            System.out.println("Q 종료");
-            System.out.println();
-            System.out.println("## 원하는 기능을 선택하세요.");
-            choice = scanner.nextLine();
-            if (choice.equals("1")) {
-                System.out.println("## 구간 관리 화면");
-                System.out.println("1. 구간 등록");
-                System.out.println("2. 구간 삭제");
-                System.out.println("B. 돌아가기");
-                System.out.println();
-                System.out.println("## 원하는 기능을 선택하세요.");
-                choice = scanner.nextLine();
-            } else if (choice.equals("2")) {
-
-            } else if (choice.equals("3")) {
-
-            } else if (choice.equals("4")) {
-
-            } else if (choice.equals("Q")) {
-                break;
-            }
+    private static void init(LineRepository lineRepository, StationRepository stationRepository) {
+        String[] nameList = {"교대역", "강남역", "역삼역", "남부터미널역", "양재역", "양재시민의숲역", "매봉역"};
+        for (String s : nameList) {
+            stationRepository.addStation(s);
         }
     }
 }
